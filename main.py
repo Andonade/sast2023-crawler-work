8295641c564f179bd0d990a38a5efaabbae7656a
Andonade <m18965691135@163.com>
2023-10-21 20:10:03 +0800

    feat(main): choose which chapters to download

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import json
 import os
 import requests
 import re
+import sys
 from bs4 import BeautifulSoup as bs
 from tqdm import tqdm
 from PIL import Image
@@ -26,8 +27,9 @@ def get_pic(url_list):
             res = requests.get(pic_url, headers=headers)
             pic_soup = bs(res.text, "lxml")
             if not pic_soup.find("a", id="eCode"):
-                filenames.append(f"pic/{part_num}/{num}.jpg")
-                with open(f"pic/{part_num}/{num}.jpg", "wb") as f:
+                pic_name = f"pic/{part_num}/{num}.jpg"
+                filenames.append(pic_name)
+                with open(pic_name, "wb") as f:
                     f.write(res.content)
             else:
                 break
@@ -48,6 +50,17 @@ def pic2pdf(filenames, book_name):
     im_list[0].save(f"{book_name}.pdf", save_all=True, append_images=im_list[1:])
 
 
+def get_part_url(href):
+    """
+    get url of picture from href.
+    :param href: url to part of the book
+    :return: picture url from href
+    """
+    part_url = f"http://reserves.lib.tsinghua.edu.cn{href}"
+    part_url = re.sub(r"index.html", "files/mobile/", part_url)
+    return part_url
+
+
 if __name__ == "__main__":
     with open("config.json", "r") as f:
         config = json.load(f)
@@ -61,9 +74,26 @@ if __name__ == "__main__":
     category = table.find_next_sibling().find_next_sibling().find_all("a")
     bookName = table.find_all("b")[1].text.strip()
     url_list = []
-    for part in category:
-        part_url = f"http://reserves.lib.tsinghua.edu.cn{part['href']}"
-        part_url = re.sub(r"index.html", "files/mobile/", part_url)
-        url_list.append(part_url)
+    if category.__len__() == 1:
+        url_list.append(get_part_url(category[0]["href"]))
+    else:
+        print("是否下载全部章节? (y/n)", end=" ")
+        choice = input()
+        if choice == "y":
+            for part in category:
+                url_list.append(get_part_url(part["href"]))
+        elif choice == "n":
+            for part in category:
+                chapter = part.text.strip()
+                print(f"是否下载 {chapter} ? (y/n)", end=" ")
+                choice = input()
+                if choice == "y":
+                    url_list.append(get_part_url(part["href"]))
+                elif choice != "n":
+                    print("非法输入!!!")
+                    sys.exit(1)
+        else:
+            print("非法输入!!!")
+            sys.exit(1)
     pic2pdf(get_pic(url_list), bookName)
     os.system("rm -rf pic")
